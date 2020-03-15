ca9674e54859c4a5fd5b5167921e44983a749bc3
chihyanglee <chihyanglee@gmail.com>
2020-03-15 12:04:11 +0800

    add Proxy pattern

---
+++ Structural/Proxy/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Subject();
+            var proxy = new Proxy(subject);
+            proxy.Request();
+        }
+    }
+    interface ISubject
+    {
+        void Request();
+    }
+    class Subject : ISubject
+    {
+        public void Request()
+        {
+
+        }
+    }
+
+    class Proxy : ISubject
+    {
+        private Subject subject;
+
+        public Proxy(Subject subject)
+        {
+            this.subject = subject;
+        }
+
+        public void Request()
+        {
+            if (Validate())
+            {
+                this.subject.Request();
+            }
+        }
+
+        public bool Validate()
+        {
+            // some rule
+            return true;
+        }
+    }
+}
