d96cb05bb564dded59414d2de3352b93883d42d7
chihyanglee <chihyanglee@gmail.com>
2020-03-15 10:40:20 +0800

    add Template pattern and Composite pattern

--- Behavioral/Template/Program.cs
+++ Behavioral/Template/Program.cs
@@ -6,7 +6,55 @@ namespace Template
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // use ConcreteClass1 operation to do TemplateMethod
+            var concrete1 = new ConcreteClass1();
+            concrete1.TemplateMethod();
+
+            // use ConcreteClass2 operation to do TemplateMethod
+            var concrete2 = new ConcreteClass2();
+            concrete2.TemplateMethod();
+        }
+    }
+    abstract class AbstractTemplate
+    {
+        public void TemplateMethod()
+        {
+            this.BaseOperation1();
+            this.CustomizedOperation1();
+            this.BaseOperation2();
+            this.CustomizedOperation2();
+        }
+
+        protected void BaseOperation1() { }
+        protected abstract void CustomizedOperation1();
+
+        protected void BaseOperation2() { }
+        protected abstract void CustomizedOperation2();
+    }
+
+    class ConcreteClass1 : AbstractTemplate
+    {
+        protected override void CustomizedOperation1()
+        {
+            // Implement CustomizedOperation1
+        }
+
+        protected override void CustomizedOperation2()
+        {
+            // Implement CustomizedOperation2
+        }
+    }
+
+    class ConcreteClass2 : AbstractTemplate
+    {
+        protected override void CustomizedOperation1()
+        {
+            // Implement CustomizedOperation1
+        }
+
+        protected override void CustomizedOperation2()
+        {
+            // Implement CustomizedOperation2
         }
     }
 }
---
+++ Structural/Composite/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Composite();
+            var branch1 = new Composite();
+            var branch1Child1 = new Leaf();
+            var branch1Child2 = new Leaf();
+            branch1.Add(branch1Child1);
+            branch1.Add(branch1Child2);
+            tree.Add(branch1);
+        }
+    }
+    abstract class Component
+    {
+        protected abstract void Operation();
+
+        public abstract void Add(Component component);
+
+        public abstract void Remove(Component component);
+
+        protected virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+
+    class Leaf : Component
+    {
+        protected override void Operation()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+        public override void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsComposite()
+        {
+            return true;
+        }
+    }
+    class Composite : Component
+    {
+        protected List<Component> children = new List<Component>();
+
+        public override void Add(Component component)
+        {
+            this.children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            this.children.Remove(component);
+        }
+        protected override void Operation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
