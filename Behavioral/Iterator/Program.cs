f6d77107e260acde0703a6afbddade80137db422
chihyanglee <chihyanglee@gmail.com>
2020-04-26 15:49:47 +0800

    add Iterator

---
+++ Behavioral/Iterator/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new WordsCollection();
+            collection.AddItem("First");
+            collection.AddItem("Second");
+            collection.AddItem("Third");
+
+            System.Console.WriteLine("Straight traversal:");
+
+            foreach (var element in collection)
+            {
+                System.Console.WriteLine(element);
+            }
+
+            System.Console.WriteLine("\nReverse traversal:");
+            collection.ReverseDirection();
+
+            foreach (var element in collection)
+            {
+                System.Console.WriteLine(element);
+            }
+        }
+    }
+    abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+        // Returns the key of the current element
+        public abstract int Key();
+        // Returns the current elemtn
+        public abstract object Current();
+        // Move forward to next element
+        public abstract bool MoveNext();
+        // Rewinds the Iterator to the first element
+        public abstract void Reset();
+    }
+
+    abstract class IteratorAggregate : IEnumerable
+    {
+        public abstract IEnumerator GetEnumerator();
+    }
+
+    class AlphabeticalOrderIterator : Iterator
+    {
+        private WordsCollection _collection;
+        private int _position = -1;
+        private bool _reverse = false;
+        public AlphabeticalOrderIterator(WordsCollection collection, bool reverse = false)
+        {
+            this._collection = collection;
+            this._reverse = reverse;
+            if (reverse)
+            {
+                this._position = collection.getItems().Count;
+            }
+        }
+        public override object Current()
+        {
+            return this._collection.getItems()[_position];
+        }
+
+        public override int Key()
+        {
+            return this._position;
+        }
+
+        public override bool MoveNext()
+        {
+            int updatedPosition = this._position + (this._reverse ? -1 : 1);
+            if (updatedPosition >= 0 && updatedPosition < this._collection.getItems().Count)
+            {
+                this._position = updatedPosition;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Reset()
+        {
+            this._position = this._reverse ? this._collection.getItems().Count - 1 : 0;
+        }
+    }
+    class WordsCollection : IteratorAggregate
+    {
+        List<string> _collection = new List<string>();
+
+        bool _direction = false;
+
+        public void ReverseDirection()
+        {
+            _direction = !_direction;
+        }
+
+        public List<string> getItems()
+        {
+            return _collection;
+        }
+        public void AddItem(string item)
+        {
+            this._collection.Add(item);
+        }
+        public override IEnumerator GetEnumerator()
+        {
+            return new AlphabeticalOrderIterator(this, _direction);
+        }
+    }
+}
