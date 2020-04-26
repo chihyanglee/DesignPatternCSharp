67d02bcf35d8889b63cb3a0087f573e4cd3e0c6e
chihyanglee <chihyanglee@gmail.com>
2020-04-26 18:16:44 +0800

    add interpreter

---
+++ Behavioral/Interpreter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context("Hello World!");
+            System.Console.WriteLine($"origin: {context.Text}");
+            var translatorA = new ExpressionToUpper();
+            translatorA.Interpret(context);
+            System.Console.WriteLine($"translatorA: {context.Text}");
+            var translatorB = new ExpressionToLower();
+            translatorB.Interpret(context);
+            System.Console.WriteLine($"translatorB: {context.Text}");
+        }
+    }
+
+    class Context
+    {
+        public string Text { get; set; }
+        public Context(string text)
+        {
+            this.Text = text;
+        }
+    }
+    interface IExpression
+    {
+        void Interpret(Context context);
+    }
+    class ExpressionToUpper : IExpression
+    {
+        public void Interpret(Context context)
+        {
+            context.Text = context.Text.ToUpper();
+        }
+    }
+    class ExpressionToLower : IExpression
+    {
+        public void Interpret(Context context)
+        {
+            context.Text = context.Text.ToLower();
+        }
+    }
+}
